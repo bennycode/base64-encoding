6fdfddf22eaa4e2228b6fba2e5bb3391e2693071
Michael Koppen <michael.koppen@googlemail.com>
2018-03-23 00:31:56 +0100

    feat: Live encoding

--- src/Root.js
+++ src/Root.js
@@ -6,7 +6,17 @@ class Root extends React.Component {
   state = {
     ascii: 'ascii',
     base64: 'base64',
+    error: null,
   }
+
+  encode = (ascii) => this.setState({...this.state, ascii, base64: btoa(ascii), error: null});
+  decode = (base64) => {
+    try {
+      this.setState({...this.state, base64, ascii: atob(base64), error: null});
+    } catch(error) {
+      this.setState({...this.state, error: error.message});
+    }
+  };
 
   onConvert = event =>
     this.setState({...this.state, ascii: this.state.base64, base64: this.state.ascii});
@@ -15,9 +25,9 @@ class Root extends React.Component {
     return (
       <div style={{width: '500px', margin: '0 auto'}}>
         <h1>Base64 Converter</h1>
-        <TextArea value={this.state.ascii} />
-        <TextArea value={this.state.base64} />
-        <input type="button" onClick={this.onConvert} value="Convert"></input>
+        <TextArea value={this.state.ascii} onChange={event => this.encode(event.target.value)} />
+        <TextArea value={this.state.base64} onChange={event => this.decode(event.target.value)} />
+        <div style={{color: 'red'}}>{this.state.error}</div>
       </div>
     );
   }
