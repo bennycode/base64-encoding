5131b6639dbbea69ccd2e557f6c0b73c345cc543
Michael Koppen <michael.koppen@googlemail.com>
2018-03-23 00:04:53 +0100

    feat: Setup component state

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 class Root extends React.Component {
+
+  state = {
+    ascii: 'ascii',
+    base64: 'base64',
+  }
+
+  onConvert = event =>
+    this.setState({...this.state, ascii: this.state.base64, base64: this.state.ascii});
+
   render() {
     return (
       <div>
-        <textarea id={'input'} defaultValue={'Input'}></textarea>
+        <textarea id="ascii" value={this.state.ascii}></textarea>
         <p></p>
-        <textarea id={'output'} defaultValue={'Output'}></textarea>
+        <textarea id="base64" value={this.state.base64}></textarea>
         <p></p>
-        <input type={'button'} id={'action'} value={'Convert'}></input>
+        <input type="button" id="action" onClick={this.onConvert} value="Convert"></input>
       </div>
     );
   }
